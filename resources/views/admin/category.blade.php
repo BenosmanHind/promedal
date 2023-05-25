9c5d697f94cf18342c0e3b9dc3bc72f0d68712b5
Benosman Hind <62807209+BenosmanHind@users.noreply.github.com>
2023-05-25 23:08:37 +0200

    category

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.dashboard-admin')
+@section('content')
+
+<div class="content-body">
+    <div class="container-fluid">
+        <div class="row page-titles mx-0">
+            <div class="col-sm-6 p-md-0">
+                <div class="welcome-text">
+                    <h4>Bonjour, Bienvenue!</h4>
+                    <span>Categories</span>
+                </div>
+            </div>
+            <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{url('/admin')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item active"><a href="{{url('/admin/categories')}}">Categories</a></li>
+                </ol>
+            </div>
+        </div>
+
+        <!-- row -->
+        <div class="row ">
+
+            <div class="col-xl-5 col-lg-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Ajouter catégorie</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <form action="{{url('admin/categories')}}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Désignation*:</label>
+                                    <input type="text"  class="form-control input-default @error('designation') is-invalid @enderror" value="{{old('designation')}}" name="designation" placeholder="designation">
+                                        @error('designation')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+                                <div class="form-group">
+                                <label>Liste des catégories :</label>
+                                    <select class="form-control  @error('category') is-invalid @enderror" id="sel1"  class="selectpicker" data-live-search="true" name="category">
+                                        <option value=0>Nothing selected</option>
+                                        @foreach($categories as $category)
+                                        <option  value="{{$category->id}}" > &nbsp &nbsp{{$category->designation}}</option>
+                                            @foreach($category->childrenCategories as $children_category)
+                                            <option  value="{{$children_category->id}}"> &nbsp &nbsp{{$children_category->designation}}</option>
+                                            @endforeach
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label >Photo : </label>
+                                    <input class="form-control" name="photo" type="file">
+                                </div>
+
+                                <div class="form-group">
+                                    <label >Description : </label>
+                                    <textarea class="form-control" name="description" rows="3"></textarea>
+                                </div>
+                                <button type="submit"  class="btn btn-primary mt-3">Ajouter</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-7">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">La table des catégories</h4>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                        <table id="example3" class="display" >
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Désignation</th>
+                                    <th>Parent Categorie</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                        <tbody>
+                            @if(isset($categories))
+                            <?php $_SESSION['i'] = 0; ?>
+                            @foreach($categories as $category)
+                                <?php $_SESSION['i']=$_SESSION['i']+1; ?>
+                                <tr id="{{$category->id}}">
+                                    <?php $dash=''; ?>
+                                    <td>{{$_SESSION['i']}}</td>
+                                    <td>{{$category->designation}}</td>
+                                    <td>
+                                        @if(isset($category->parent_id))
+                                            {{$category->childCategories->designation}}
+                                        @else
+                                        <i class="fa-solid fa-minus"></i>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <form action="{{url('admin/categories/'.$category->id)}}" method="post">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                        <div class="d-flex">
+                                            <a href="{{url('admin/categories/'.$category->id.'/edit')}}" class="btn btn-primary shadow btn-xs sharp mr-1"><i class="fa fa-pencil"></i></a>
+                                            <button class="  btn btn-danger shadow btn-xs sharp" onclick="return confirm('Vous voulez vraiment supprimer?')"><i class="fa fa-trash"></i></button>
+                                        </div>
+                                        </form>
+                                    </td>
+                                 </tr>
+                                 @if(count($category->childCategories))
+                                     @include('sub-category-list',['subcategories' => $category->childCategories])
+                                 @endif
+
+                            @endforeach
+                            <?php unset($_SESSION['i']); ?>
+                             @endif
+                        </tbody>
+                        </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+</div>
+</div>
+@endsection
